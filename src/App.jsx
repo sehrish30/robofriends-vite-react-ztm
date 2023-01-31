a2e2b5cdd8c177fd00dc932c38a3e6e6e2185c89
sehrish30 <sehrishwaheed98@gmail.com>
2023-01-31 10:26:59 +0300

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
-import { robots } from "./services/robots";
+
 import "tachyons";
 import CardList from "./components/card-list";
 import SearchBox from "./components/searchbox.component";
+import Scroll from "./components/scroll.component";
 
 class App extends React.Component {
   constructor() {
@@ -12,9 +13,20 @@ class App extends React.Component {
 
     // state is something that can change
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: "",
     };
+  }
+
+  // the component is mounted and its rendered
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({
+          robots: users,
+        });
+      });
   }
 
   // here this value is according to where it was
@@ -25,19 +37,26 @@ class App extends React.Component {
       searchField: event.target.value,
     });
   };
+
   render() {
     const filterrobots = this.state.robots.filter((robot) => {
       return robot.name
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
     });
-    return (
-      <React.Fragment>
-        <h1>Robo friends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filterrobots} />
-      </React.Fragment>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>;
+    } else {
+      return (
+        <React.Fragment>
+          <h1>Robo friends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList robots={filterrobots} />
+          </Scroll>
+        </React.Fragment>
+      );
+    }
   }
 }
 
